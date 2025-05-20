4e32ad1b5d215d7c788e4b81a63579613938fc85
Charlotte Chaze <charlotte@howtobreakintotech.com>
2025-05-20 14:53:57 -0400

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+export default {
+  async fetch(request, env, ctx) {
+    return new Response("Hello from your Cloudflare Worker!", {
+      headers: { "content-type": "text/plain" },
+    });
+  },
+};
